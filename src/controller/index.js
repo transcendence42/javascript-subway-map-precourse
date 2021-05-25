8980a62f1df91cdd45a3afe3ce46897b1b7e57d1
hochan Lee <hochan049@gmail.com>
2021-05-25 16:04:05 +0900

    feat: add toggleDisplayLineManagement

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,9 @@
 import { controlStationManagement } from './station-management/index.js';
+import { controlLineManagement } from './line-management/index.js';
 import { controlSubwayRouteMap } from './subway-route-map/index.js';
 
 export const controller = () => {
   controlStationManagement();
+  controlLineManagement();
   controlSubwayRouteMap();
 };
---
+++ src/controller/line-management/index.js
@@ -0,0 +1,14 @@
+import { elementIds, addButtonEvent } from '../utils.js';
+
+function toggleDisplayLineManagement() {
+  elementIds.stationManagement.hidden = true;
+  elementIds.lineManagement.hidden = !elementIds.lineManagement.hidden;
+  elementIds.sectionManagement.hidden = true;
+  if (elementIds.mapPrintManagement) {
+    elementIds.mapPrintManagement.hidden = true;
+  }
+}
+
+export const controlLineManagement = () => {
+  addButtonEvent(elementIds.lineManagerButton, toggleDisplayLineManagement);
+};
