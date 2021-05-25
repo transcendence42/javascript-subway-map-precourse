79904a5fdee13ac96d8f5f14910f704316fce2b3
hochan Lee <hochan049@gmail.com>
2021-05-25 16:07:12 +0900

    feat: add toggleSectionManagement page

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,11 @@
 import { controlStationManagement } from './station-management/index.js';
 import { controlLineManagement } from './line-management/index.js';
+import { controlSectionManagement } from './section-management/index.js';
 import { controlSubwayRouteMap } from './subway-route-map/index.js';
 
 export const controller = () => {
   controlStationManagement();
   controlLineManagement();
+  controlSectionManagement();
   controlSubwayRouteMap();
 };
---
+++ src/controller/section-management/index.js
@@ -0,0 +1,14 @@
+import { elementIds, addButtonEvent } from '../utils.js';
+
+function toggleSectionManagement() {
+  elementIds.stationManagement.hidden = true;
+  elementIds.lineManagement.hidden = true;
+  elementIds.sectionManagement.hidden = !elementIds.sectionManagement.hidden;
+  if (elementIds.mapPrintManagement) {
+    elementIds.mapPrintManagement.hidden = true;
+  }
+}
+
+export const controlSectionManagement = () => {
+  addButtonEvent(elementIds.sectionManagerButton, toggleSectionManagement);
+};
