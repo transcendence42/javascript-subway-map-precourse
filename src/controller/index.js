044d5ad70f56c0884cb1ce70fa361ad948941ede
hochan Lee <hochan049@gmail.com>
2021-05-25 15:53:15 +0900

    feat: add controlSubwayRouteMap frame

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,5 +1,7 @@
 import { controlStationManagement } from './station-management/index.js';
+import { controlSubwayRouteMap } from './subway-route-map/index.js';
 
 export const controller = () => {
   controlStationManagement();
+  controlSubwayRouteMap();
 };
