b470d5753c7f9b324848cdf13678f337f4fd149a
hochan Lee <hochan049@gmail.com>
2021-05-26 16:20:43 +0900

    feat: add checkValidLineSelector

--- src/controller/line-management/index.js
+++ src/controller/line-management/index.js
@@ -1,6 +1,7 @@
 import { addButtonEvent } from '../utils.js';
 import { storage } from '../../model/index.js';
 import { elementIds } from '../../utils.js';
+import {errorMessage} from '../error-message.js';
 
 const toggleDisplayLineManagement = () => {
   elementIds.stationManagement.hidden = true;
@@ -11,13 +12,29 @@ const toggleDisplayLineManagement = () => {
   }
 };
 
-const addLine = () => {
-  const lineName = elementIds.lineNameInput;
-  // const lineStartStationSelector = elementIds.lineStartStationSelector;
-  // const lineEndStationSelector = elementIds.lineEndStationSelector;
+const checkValidLineSelector = (lineStartStationSelectorValue, lineEndStationSelectorValue) => {
+  if (lineStartStationSelectorValue === lineEndStationSelectorValue) {
+    alert(errorMessage.stationDuplicate);
+    return false;
+  } 
+  return true;
+  // TODO storage compare
+}
 
-  console.log(storage.getLocalStorageMap('subway-station'))
-  console.log(lineName.value);
+const addLine = () => {
+  const lineStartStationSelectorValue = elementIds.lineStartStationSelector.value;
+  const lineEndStationSelectorValue = elementIds.lineEndStationSelector.value;
+  if (!checkValidLineSelector(lineStartStationSelectorValue, lineEndStationSelectorValue)) {
+    return ;
+  }
+
+  let line = new Map();
+  line.set(elementIds.lineNameInput.value , {
+    lineStartStationSelectorValue,
+    lineEndStationSelectorValue,
+  })
+  console.log(line)
+  storage.setLocalStorageMap('subway-line', line);
 };
 
 export const controlLineManagement = () => {
