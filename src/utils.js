3f8ababb8288d5aa5269dbb10a3e906b5410e770
hochan Lee <hochan049@gmail.com>
2021-05-26 13:19:30 +0900

    fix: elementIds button id to class, && feat: getElementClass

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,16 @@ const getElementId = id => {
   return document.getElementById(id);
 };
 
+const getElementClass = className => {
+  return document.getElementsByClassName(className);
+};
+
 export const elementIds = {
   stationManagement: getElementId('station-management'),
   stationManagerButton: getElementId('station-manager-button'),
   stationNameInput: getElementId('station-name-input'),
   stationAddButton: getElementId('station-add-button'),
-  stationDeleteButton: getElementId('station-delete-button'),
+  stationDeleteButton: getElementClass('station-delete-button'),
   stationTableTbody: document.querySelector('tbody'),
   lineManagement: getElementId('line-management'),
   lineManagerButton: getElementId('line-manager-button'),
@@ -15,14 +19,14 @@ export const elementIds = {
   lineStartStationSelector: getElementId('line-start-station-selector'),
   lineEndStationSelector: getElementId('line-end-station-selector'),
   lineAddButton: getElementId('lineAddButton'),
-  lineDeleteButton: getElementId('lineDeleteButton'),
+  lineDeleteButton: getElementClass('lineDeleteButton'),
   sectionManagerButton: getElementId('section-manager-button'),
   sectionManagement: getElementId('section-management'),
   sectionLineMenuButton: getElementId('section-line-menu-button'),
   sectionStationSelector: getElementId('section-station-selector'),
   sectionOrderInput: getElementId('section-order-input'),
   sectionAddButton: getElementId('section-add-button'),
-  sectionDeleteButton: getElementId('section-delete-button'),
+  sectionDeleteButton: getElementClass('section-delete-button'),
   mapPrintManagement: getElementId('map-print-management'),
   mapPrintManagerButton: getElementId('map-print-manager-button'),
 };
