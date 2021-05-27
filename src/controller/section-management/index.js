be6b82648261cfe03879bc3946e286128941cc88
hochan Lee <hochan049@gmail.com>
2021-05-27 12:54:22 +0900

    feat: add select options for section management

--- src/controller/section-management/index.js
+++ src/controller/section-management/index.js
@@ -1,5 +1,6 @@
 import { addButtonEvent } from '../utils.js';
 import { elementIds } from '../../utils.js';
+import { storage } from '../../model/index.js';
 
 const toggleSectionManagement = () => {
   elementIds.stationManagement.hidden = true;
@@ -8,18 +9,40 @@ const toggleSectionManagement = () => {
   if (elementIds.mapPrintManagement) {
     elementIds.mapPrintManagement.hidden = true;
   }
-}
+};
 
 const renderSelectionRegister = (lineName) => {
-  elementIds.sectionManageText.innerHTML = "";
-  elementIds.sectionManageText.insertAdjacentHTML('afterbegin', `<h3>${lineName}</h3>`);
+  elementIds.sectionManageText.innerHTML = '';
+  elementIds.sectionManageText.insertAdjacentHTML(
+    'afterbegin',
+    `<h3>${lineName}</h3>`,
+  );
   elementIds.sectionRegister.hidden = false;
-}
+};
+
+const addSelectOption = (element, stations) => {
+  stations.forEach(station => {
+    let option = document.createElement('option');
+    option.value = station;
+    option.innerHTML = station;
+    element.appendChild(option);
+  });
+};
+
+const renderSelect = (lineName) => {
+  console.log(storage.getLocalStorageMap('subway-line'));
+  for (let item of storage.getLocalStorageMap('subway-line')) {
+    if (lineName === item[0]) {
+      addSelectOption(elementIds.sectionStationSelector, item[1]);
+    }
+  }
+};
 
 const selectSectionLineButton = (e) => {
   const lineName = e.currentTarget.dataset.line;
   renderSelectionRegister(lineName);
-}
+  renderSelect(lineName);
+};
 
 export const controlSectionManagement = () => {
   addButtonEvent(elementIds.sectionManagerButton, toggleSectionManagement);
--- src/view/section-management/index.js
+++ src/view/section-management/index.js
@@ -9,23 +9,6 @@ const renderLineButtons = () => {
   elementIds.sectionButtons.insertAdjacentHTML('afterend', result);
 };
 
-// const addSelectOption = (element, value) => {
-//   let option = document.createElement('option');
-//   option.value = value;
-//   option.innerHTML = value;
-//   element.appendChild(option);
-// };
-
-// const renderSelect = () => {
-//   for (let item of storage.getLocalStorageMap('subway-station')) {
-//     addSelectOption(elementIds.lineStartStationSelector, item[0]);
-//   }
-//   for (let item of storage.getLocalStorageMap('subway-station')) {
-//     addSelectOption(elementIds.lineEndStationSelector, item[0]);
-//   }
-// };
-
 export const renderSectionManagement = () => {
   renderLineButtons();
-  // renderSelect();
 };
