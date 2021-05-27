1405112f9cade8e57d836a631adc3fd39b93367d
hochan Lee <hochan049@gmail.com>
2021-05-27 12:19:56 +0900

    feat: 구간관리에서 노선 버튼 view 부분 완성

--- src/view/section-management/index.js
+++ src/view/section-management/index.js
@@ -1,15 +1,16 @@
 import { storage } from '../../model/index.js';
 
 const renderLineButtons = () => {
-    let result = ''
-   for (let item of storage.getLocalStorageMap('subway-line').keys()) {
-       document.getElementById('section-buttons').insertAdjacentHTML('afterend', `<button class="section-line-menu-button">${item}</button>`);
-    //  result += `<button class="section-line-menu-button">${item}</button> `
-   }
-
+  let result = '';
+  for (let item of storage.getLocalStorageMap('subway-line').keys()) {
+    result += `<button class="section-line-menu-button">${item}</button> `;
+  }
+  document
+    .getElementById('section-buttons')
+    .insertAdjacentHTML('afterend', result);
 };
 
-{/* <button class="section-line-menu-button">호선</button> */}
+
 
 export const renderSectionManagement = () => {
   renderLineButtons();
