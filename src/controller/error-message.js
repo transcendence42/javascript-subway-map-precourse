91870f7d86b1a84ff80fa545c6def05ddc9b4963
hochan Lee <hochan049@gmail.com>
2021-05-27 13:51:01 +0900

    feat: 알맞은 option과 input 확인하는 checkValid 함수 구현

--- src/controller/error-message.js
+++ src/controller/error-message.js
@@ -3,4 +3,6 @@ export const errorMessage = {
   stationLength: '두글자 미만',
   lineDuplicate: '노선 중복',
   lineStationForbidden: '노선에 등록된 역은 삭제할 수 없습니다.',
+  sectionInvalidRangeInput: '범위에 맞지 않은 입력입니다. 재입력 해주세요!',
+  sectionInvalidOption: '노선에 이미 등록된 역입니다. 재입력 해주세요!',
 };
--- src/controller/section-management/index.js
+++ src/controller/section-management/index.js
@@ -1,6 +1,7 @@
 import { addButtonEvent } from '../utils.js';
 import { elementIds } from '../../utils.js';
 import { storage } from '../../model/index.js';
+import {errorMessage} from '../error-message.js';
 
 const toggleSectionManagement = () => {
   elementIds.stationManagement.hidden = true;
@@ -69,11 +70,51 @@ const selectSectionLineButton = (e) => {
   renderSelect(lineName);
 };
 
-{/* <button class="section-delete-button">노선에서 제거</button> */}
+const findSubwayLineTag = () => {
+  for (let item of storage.getLocalStorageMap('subway-line')) {
+    if (item[0] === document.getElementsByTagName('h3')[1].innerHTML) {
+      return item;
+    }
+  }  
+  return ;
+}
+
+const checkValidInput = (stationLength, sectionOrderInputValue) => {
+  return stationLength >= sectionOrderInputValue && sectionOrderInputValue >= 0
+}
+
+const checkValidOption = (lineStation, sectionStationOption) => {
+  return !lineStation.includes(sectionStationOption)
+}
+
+const initSectionInput = (message) => {
+  alert(message);
+  elementIds.sectionOrderInput.value = ''
+  elementIds.sectionOrderInput.focus()
+}
+
+const addSectionLineStation = () => {
+  let subwayLine;
+  const sectionOrderInputValue = elementIds.sectionOrderInput.value
+  const sectionStationOption = elementIds.sectionStationSelector.value
+
+  subwayLine = findSubwayLineTag();
+  if (!checkValidInput(subwayLine[1].length, Number(sectionOrderInputValue))) {
+    initSectionInput(errorMessage.sectionInvalidRangeInput)
+    return ;
+  }
+  if (!checkValidOption(subwayLine[1], sectionStationOption)) {
+    initSectionInput(errorMessage.sectionInvalidOption)
+    return ;
+  }
+  // console.log(subwayLine.values())
+  // if (subwayLine.values)
+}
 
 export const controlSectionManagement = () => {
   addButtonEvent(elementIds.sectionManagerButton, toggleSectionManagement);
   for (let item of elementIds.sectionLineMenuButton) {
     addButtonEvent(item, selectSectionLineButton);
   }
+  addButtonEvent(elementIds.sectionAddButton, addSectionLineStation);
 };
