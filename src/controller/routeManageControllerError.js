735fa9f8f42b285b9a0ab7c9d4125d14eda00044
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-05-28 15:07:16 +0900

    feat: add error msg about route

---
+++ src/controller/routeManageControllerError.js
@@ -0,0 +1,14 @@
+export const ERROR_CODE = {
+  SUCCESS: 0,
+  EMPTY_ROUTE_NAME: 1,
+  WRONG_ROUTE_NAME: 2,
+  EQUAL_UPWARD_AND_DOWNWARD: 3,
+  ROUTE_NAME_DUP: 4
+};
+
+export const ERROR_CODE_MSG = {
+  1: "노선 이름이 비어있습니다.",
+  2: "노선 이름이 잘못되었습니다.",
+  3: "상행 종점과 하행 종점이 같습니다.",
+  4: "노선 이름이 중복됩니다."
+};
