f7d30e18b6bc3b9fd9b2d7dcc96329e6cf31efa1
hochan Lee <hochan049@gmail.com>
2021-05-28 13:43:32 +0900

    chore: view page, modify '/ to '

--- src/view/line-management/index.js
+++ src/view/line-management/index.js
@@ -18,12 +18,12 @@ const renderSelect = () => {
 };
 
 const addLineTable = ({ lineName, value }) => {
-  return `<tr data-line=\'${lineName}\'>
+  return `<tr data-line='${lineName}'>
             <td>${lineName}</td>
             <td>${value[0]}</td>
             <td>${value[value.length - 1]}</td>
             <td>
-              <button class='line-delete-button' data-line=\'${lineName}-button\'>삭제</button>
+              <button class='line-delete-button' data-line='${lineName}-button'>삭제</button>
             </td>
            </tr>`;
 };
