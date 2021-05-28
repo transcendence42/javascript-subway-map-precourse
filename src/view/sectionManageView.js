f9a06ff5f6af5366dc00a0c69225009f81966c57
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-05-28 20:20:29 +0900

    feat: add html class to element

--- src/view/sectionManageView.js
+++ src/view/sectionManageView.js
@@ -12,13 +12,14 @@ export default class SectionManageView {
     let ret = `<h2>${routeName} 관리</h2>
       <h3>구간 등록</h3>
       <p>
-        <select>`;
+        <select class="manage-section">`;
     stationsExceptRouteStations.forEach(stationName => {
       ret += `<option>${stationName}</option>`;
     });
+
     ret += `</select>
         <input type="number" min="0" max="${routeStations.length}">
-        <button>등록</button>
+        <button class="register-section-button">등록</button>
       </p>
       <table>
           <tbody>
@@ -28,7 +29,7 @@ export default class SectionManageView {
                   <th>설정</th>
               </tr>`;
     routeStations.forEach((stationName, idx) => {
-      ret += `<tr><td>${idx}</td><td>${stationName}</td><td><button>노선에서 제거</button></td></tr>`;
+      ret += `<tr><td>${idx}</td><td>${stationName}</td><td><button class="delete-route-station">노선에서 제거</button></td></tr>`;
     });
     ret += `</tbody></table>`;
     document.querySelector("#manage-section").innerHTML = ret;
