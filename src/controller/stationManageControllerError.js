4812415abdc0eeaaa2de7f01145a6bafb61f0b22
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-05-28 15:16:44 +0900

    feat: add error about adding station

---
+++ src/controller/stationManageControllerError.js
@@ -0,0 +1,12 @@
+export const ERROR_CODE = {
+  SUCCESS: 0,
+  EMPTY_STATION_NAME: 1,
+  WRONG_STATION_NAME: 2,
+  STATION_NAME_DUP: 3
+};
+
+export const ERROR_CODE_MSG = {
+  1: "역 이름이 비어있습니다.",
+  2: "역 이름이 잘못되었습니다.",
+  3: "역 이름이 중복됩니다."
+};
