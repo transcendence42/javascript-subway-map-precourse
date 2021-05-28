8f0f86cfe6a8e0c75a6c656eae3b5d0164d985be
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-05-28 13:07:53 +0900

    feat: add event to add route button

--- src/controller/routeManageController.js
+++ src/controller/routeManageController.js
@@ -1,4 +1,5 @@
 import RouteManageView from "../view/routeManageView.js";
+import StationDAO from "../model/stationDAO.js";
 
 export default class RouteManageController {
   constructor() {
@@ -8,6 +9,20 @@ export default class RouteManageController {
   addEventAboutShowAllRoutes() {
     document
       .querySelector("#line-manager-button")
-      .addEventListener("click", () => this.routeManageView.showAllRoutes());
+      .addEventListener("click", () => {
+        this.routeManageView.showAllRoutes();
+        this.addEventAboutAddRoute();
+      });
+  }
+  addEventAboutAddRoute() {
+    document.querySelector("#add-route").addEventListener("click", () => {
+      let routeName = document.querySelector("#route-name").value;
+      let upwardEndStation = document.querySelector("[name=upward-end-station]")
+        .value;
+      let downwardEndStation = document.querySelector(
+        "[name=downward-end-station]"
+      ).value;
+      console.log(routeName, upwardEndStation, downwardEndStation);
+    });
   }
 }
