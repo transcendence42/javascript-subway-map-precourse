a54fe7d79e77337c6577e779d625226aa1a902d2
hochan Lee <hochan049@gmail.com>
2021-05-28 10:49:51 +0900

    refactor: error-message

--- src/controller/error-message.js
+++ src/controller/error-message.js
@@ -1,8 +1,8 @@
 export const errorMessage = {
-  stationDuplicate: '역 중복',
-  stationLength: '두글자 미만',
-  lineDuplicate: '노선 중복',
-  lineStationForbidden: '노선에 등록된 역은 삭제할 수 없습니다.',
-  sectionInvalidRangeInput: '범위에 맞지 않은 입력입니다. 재입력 해주세요!',
-  sectionInvalidOption: '노선에 이미 등록된 역입니다. 재선택 해주세요!',
+  stationDuplicate: '❗역이 중복되었습니다. ❗',
+  stationLength: '❗두글자 미만으로는 설정 할 수 없습니다.❗',
+  lineDuplicate: '❗중복된 노선입니다. ❗',
+  lineStationForbidden: '❗노선에 등록된 역은 삭제할 수 없습니다.❗',
+  sectionInvalidRangeInput: '❗범위에 맞지 않은 입력입니다. 재입력 해주세요!❗',
+  sectionInvalidOption: '❗노선에 이미 등록된 역입니다. 재선택 해주세요!❗',
 };
